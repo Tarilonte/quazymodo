cc0f7706769d2de61ae4bccb7d4e16e8da7ae82e
Tarik Tarilonte <tariktarilonte@gmail.com>
2026-02-15 18:56:09 -0300

    feat(home-sections): ajustar layout em metades e integrar theme selector

--- app/components/pages/home-sections/home-sections.blueprint.php
+++ app/components/pages/home-sections/home-sections.blueprint.php
@@ -14,6 +14,12 @@ return [
   ],
   'inserts' => [
     'page-title' => APP_NAME . ' - Full Sections',
-    'body' => componentFactory::Template(componentName: '/pages/home-sections/'),
+    'body' => [
+      componentFactory::Plugin(
+        componentName: '/plugins/theme-selector/',
+        inserts: ['btn-themeSelector-01-css' => 'fixed top-0 right-0 m-8 z-50']
+      ),
+      componentFactory::Template(componentName: '/pages/home-sections/'),
+    ],
   ]
 ];
